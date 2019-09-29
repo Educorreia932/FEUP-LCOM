a21d13f9ce24082497165c2430f787848bd117f1
up201806516 <up201806516@1742a6f7-5c60-4130-9989-5d8d78b829ae>
2019-09-29 22:20:21 +0000

    • Added status message related macros. • Wrote several functions: - util_sys_inb() - timer_display_config() - timer_get_conf() • Added auxiliary timer_get_port() function. NOTE: Passed all timer_test_read_config() tests at the time of this commit. WARNING: util_get_msb() and util_get_lsb() are untested. Still need to check if the given arguments are within the expected range.

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -30,10 +30,10 @@ int main(int argc, char *argv[]) {
 }
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  uint8_t st; 
+  if (timer_get_conf(timer, &st))
+    return 1;
+  return timer_display_conf(timer, st, field);
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
--- lab2/utils.c
+++ lab2/utils.c
@@ -3,22 +3,20 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  // How can it ever fail?
+  *lsb = (uint8_t) (val & 0x00FF);
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  // How can it ever fail?
+  val = val >> 8;
+  *msb = (uint8_t) val;
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  // Check if it dies later
+  u32_t *value32 = (u32_t *) value;
+  return sys_inb(port, value32);
 }
