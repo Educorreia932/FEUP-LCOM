38833069e628cb3b839ed448aa5d6b8787ca8330
up201806516 <up201806516@1742a6f7-5c60-4130-9989-5d8d78b829ae>
2019-09-30 16:34:44 +0000

    • Fixed timer_set_frequency() • Removed an unnecessary logical & util_get_LSB() • Fixed a bug in util_sys_inb() where it could write into memory where it shouldn't • Added timer value verification in timer_display_conf() NOTE: The previous commit did NOT pass the tests

--- lab2/utils.c
+++ lab2/utils.c
@@ -4,7 +4,7 @@
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
   // How can it ever fail?
-  *lsb = (uint8_t) (val & 0x00FF);
+  *lsb = (uint8_t) val;
   return 0;
 }
 
@@ -17,6 +17,9 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 
 int (util_sys_inb)(int port, uint8_t *value) {
   // Check if it dies later
-  u32_t *value32 = (u32_t *) value;
-  return sys_inb(port, value32);
+  u32_t value32;
+  if (sys_inb(port, &value32))
+    return 1;
+  *value = (uint8_t) value32;
+  return 0;
 }
