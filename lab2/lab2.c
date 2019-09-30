e069f2b06eb6e7e9137b676815ae70f1aa81ccf5
Tiago Duarte da Silva <48838198+tiagodusilva@users.noreply.github.com>
2019-09-30 19:16:33 +0000

    • Added timer_test_int(), timer_subscribe_int(),  timer_unsubscribe_int() and timer_int_handler() • Added two global vars to timer.c, timer0_hook_id and global_counter (the latter is used as an extern on lab2.c) NOTE: Possible bug on test [1, 9], all others pass

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+extern unsigned int global_counter;
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -40,9 +41,46 @@ int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
     return timer_set_frequency(timer, freq);
 }
 
+// Assuming, as we were told, that frequency is already set to 60
 int(timer_test_int)(uint8_t time) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
+  uint8_t bit_no;
+
+  if (timer_subscribe_int(&bit_no))
+    return 1;
+
+  int r;
+  int ipc_status;
+  message msg;
+
+  // Time only diminishes after a second has elapsed
+  while( time ) { /* You may want to use a different condition */
+    /* Get a request message. */
+    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
+        printf("driver_receive failed with: %d", r);
+        continue;
+    }
+    if (is_ipc_notify(ipc_status)) { /* received notification */
+        switch (_ENDPOINT_P(msg.m_source)) {
+            case HARDWARE: /* hardware interrupt notification */				
+                if (msg.m_notify.interrupts & BIT(bit_no)) { /* subscribed interrupt */
+                  timer_int_handler();
+                  if (global_counter == 60)
+                  {
+                    timer_print_elapsed_time();
+                    global_counter = 0;
+                    --time;
+                  }
+                }
+                break;
+            default:
+                break; /* no other notifications expected: do nothing */	
+        }
+    } else { /* received a standard message, not a notification */
+        /* no standard messages expected: do nothing */
+    }
+  }
+
+  timer_unsubscribe_int();
+  return 0;
 }
