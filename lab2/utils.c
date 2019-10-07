299b6ffb2356d17d32f19c39cb8c3af6ccc38b1f
Tiago Duarte da Silva <48838198+tiagodusilva@users.noreply.github.com>
2019-10-07 17:07:23 +0000

    Added more comments and improved some code.

--- lab2/utils.c
+++ lab2/utils.c
@@ -20,9 +20,10 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 int (util_sys_inb)(int port, uint8_t *value) {
   if (!value) // Verify if pointer is NULL
     return 1;
-  u32_t value32;
+
+  u32_t value32; // Aux u32_t to avoid errors in the sys_inb call
   if (sys_inb(port, &value32))
     return 1;
-  *value = (uint8_t) value32;
+  *value = (uint8_t) value32; // Copies the result of the call to the 8 bit argument
   return 0;
 }
