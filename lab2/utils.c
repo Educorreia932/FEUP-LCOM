d24b51aeab74289bdfd3f9727a963e32a65c463e
up201806516 <up201806516@1742a6f7-5c60-4130-9989-5d8d78b829ae>
2019-10-07 14:54:09 +0000

    Added verification for NULL pointers (some still missing verification). Now verifies if the freq argument in timer_set_freq() is >= TIMER_FREQ (the clock), in which case returns 1.

--- lab2/utils.c
+++ lab2/utils.c
@@ -3,14 +3,14 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  if (!lsb)
+  if (!lsb) // Verify if pointer is NULL
     return 1;
   *lsb = (uint8_t) val;
   return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  if (!msb)
+  if (!msb) // Verify if pointer is NULL
     return 1;
   val = val >> 8;
   *msb = (uint8_t) val;
@@ -18,6 +18,8 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
+  if (!value) // Verify if pointer is NULL
+    return 1;
   u32_t value32;
   if (sys_inb(port, &value32))
     return 1;
