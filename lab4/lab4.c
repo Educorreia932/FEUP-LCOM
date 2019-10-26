cb98d764102c382c08f56931413e3b40312c44cb
Eduardo Correia <44172778+Educorreia932@users.noreply.github.com>
2019-10-26 15:09:35 +0000

    Started to develop mouse_test_packet() Implemented the loop to print packets and the mouse interrupt handler Imported keyboard macros, auxiliary functions and methods NOTE: It doesn't pass the tests yet

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 
 // Any header files included below this line should have been created by you
+#include "i8042.h"
+#include "mouse.h"
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -30,11 +32,66 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-
 int (mouse_test_packet)(uint32_t cnt) {
-    /* To be completed */
-    printf("%s(%u): under construction\n", __func__, cnt);
-    return 1;
+    uint8_t bit_no = MOUSE_IRQ;
+
+    // Only avoids making this operation on every notification
+    int mouse_bit_mask = BIT(bit_no);
+
+	//if (kbc_send_cmd(0x64, STREAM_MODE))
+	//	return 1;
+
+	//if (kbc_send_cmd(0x64, EN_DATA_REP))
+	//	return 1;
+
+	if (mouse_enable_data_reporting())
+		return 1;
+
+	if (mouse_subscribe_int(&bit_no))
+		return 1;
+
+	int r, ipc_status;
+  	message msg;
+
+	// Interrupt loop
+	while (cnt) { 
+			if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+				printf("driver_receive failed with: %d", r);
+				continue;
+			}
+
+			if (is_ipc_notify(ipc_status)) {
+				switch (_ENDPOINT_P(msg.m_source)) {
+					case HARDWARE: /* hardware interrupt notification */
+						if (msg.m_notify.interrupts & mouse_bit_mask) { /* subscribed interrupt */
+                            mouse_ih();
+														
+							if (error)
+                                return 1;
+														
+                            if (read_data())
+                                return 1;  
+
+                            mouse_print_packet(&data);
+
+                            cnt--;                 
+						}
+
+						break;
+					default:
+						break; /* no other notifications expected: do nothing */     
+				}
+			}
+
+			else { /* received a standard message, not a notification */
+				/* no standard messages expected: do nothing */
+			}
+	}
+
+	if (mouse_unsubscribe_int(&bit_no))
+		return 1;
+
+	return 0;
 }
 
 int (mouse_test_remote)(uint16_t period, uint8_t cnt) {
@@ -49,7 +106,7 @@ int (mouse_test_async)(uint8_t idle_time) {
     return 1;
 }
 
-int (mouse_test_gesture)() {
+int (mouse_test_gesture)(uint8_t x_len, uint8_t tolerance) {
     /* To be completed */
     printf("%s: under construction\n", __func__);
     return 1;
