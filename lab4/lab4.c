654d4ce34adac0b4d317df21b09541e75b7bc3c9
up201806516 <up201806516@1742a6f7-5c60-4130-9989-5d8d78b829ae>
2019-10-29 19:53:58 +0000

    Added mouse_test_remote() Commented all code. Added new mouse macros. Fixed a problem with sending mouse commands, because in case of NACK, we needed to try the whole sequence again. Note: Passes all tests so far TIME FOR GESTURE!

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -99,26 +99,89 @@ int (mouse_test_packet)(uint32_t cnt) {
 }
 
 int (mouse_test_remote)(uint16_t period, uint8_t cnt) {
-  /* To be completed */
-  printf("%s(%u, %u): under construction\n", __func__, period, cnt);
-  return 1;
+  
+
+  // Set mouse to remote mode and disable data reporting
+  if (mouse_set_remote_mode())
+    return 1;
+  if (mouse_data_reporting_disable())
+    return 1;
+
+  uint8_t status;
+  uint8_t bytes_read;
+  while (cnt) {
+    
+    // Send cmd to read a packet to the mouse
+    if (mouse_read_data())
+      return 1;
+
+    bytes_read = 3;
+    while (bytes_read) { // We will read 3 bytes (a complete packet)
+
+      // Read status
+      if (util_sys_inb(MOUSE_STAT_REG, &status))
+        return 1;
+      
+      // Check if the output buffer is full with mouse data
+      if (status & (ST_MOUSE_DATA | ST_OUT_BUF)) {
+
+        // Pretty much same treatment as with the interrupts
+        mouse_ih();
+                  
+        if (mouse_ih_error)
+          return 1;
+      
+        if (mouse_data_handler())
+          return 1;
+
+        --bytes_read;
+      }
+    }
+    
+    if (is_mouse_packet_complete) {
+      cnt--;
+      // printf("%x, %x, %x\n", mouse_parsed_packet.bytes[0], mouse_parsed_packet.bytes[1], mouse_parsed_packet.bytes[2]);
+      mouse_print_packet(&mouse_parsed_packet);
+    }
+
+    // We have read out packet, so we wait for period miliseconds
+    tickdelay(micros_to_ticks(period * 1000)); // *1000 to convert to microseconds
+  }
+
+  // Reenable stream mode
+  if (mouse_set_stream_mode())
+    return 1;
+  
+  // Disable data reporting
+  if (mouse_data_reporting_disable())
+    return 1;
+
+  // Restore KBC byte to default state (returned by minix_get_dflt_kbc_byte())
+  if (kbc_send_cmd(IN_BUF_CMD, WRITE_CMD_BYTE))
+    return 1;
+  if (kbc_send_cmd(IN_BUF_ARGS, minix_get_dflt_kbc_cmd_byte()))
+    return 1;
+
+  return 0;
 }
 
 int (mouse_test_async)(uint8_t idle_time) {
   uint8_t mouse_bit_no = MOUSE_IRQ;
   uint8_t timer0_bit_no = TIMER0_IRQ;
 
+  // Subscribe to timer0
   if (timer_subscribe_int(&timer0_bit_no))
     return 1;
 
+  // Set mouse to streaming mode with data reporting enabled
   if (mouse_set_stream_mode())
     return 1;
-
 	if (mouse_data_reporting_enable())
 	 	return 1;
   
+  // Subscribe to mouse interrupts
 	if (mouse_subscribe_int(&mouse_bit_no)) {
-		timer_unsubscribe_int();
+		timer_unsubscribe_int(); // If it fails, we need to unsubscribe from timer0
     return 1;
   }
 
@@ -180,12 +243,15 @@ int (mouse_test_async)(uint8_t idle_time) {
 			}
 	}
 
+  // Unsubscribe from timer0 interrupts
   if (timer_unsubscribe_int())
     return 1;
 
-	if (mouse_unsubscribe_int(&mouse_bit_no))
+  // Unsubscribe from mouse interrupts
+	if (mouse_unsubscribe_int())
 		return 1;
 
+  // Mouse is already on streaming mode, only need to disable data reporting
   if (mouse_data_reporting_disable())
     return 1;
 
--- lab4/mouse.c
+++ lab4/mouse.c
@@ -30,31 +30,55 @@ uint8_t (mouse_unsubscribe_int)() {
     return 0;
 }
 
+// Has a more complicated structure than simple KBC commands,
+// so this function handles that interaction
 uint8_t mouse_send_cmd(uint8_t cmd) {
-    if (kbc_send_cmd(MOUSE_CTRL_REG, KBC_WRITE_BYTE_TO_MOUSE))
-        return 1;
-    if (kbc_send_cmd(MOUSE_IN_BUF, cmd))
-        return 1;
 
-    uint8_t byte_received;
-    if (kbc_read_outbf(MOUSE_OUT_BUF, &byte_received) || byte_received != MOUSE_CTRL_ACK)
+    // If a command's response is NACK, we need to retry all over again
+    uint8_t attempts = 3;
+    while (attempts) {
+
+        // Write cmd to tell the KBC that we'll send a mouse cmd
+        if (kbc_send_cmd(MOUSE_CTRL_REG, KBC_WRITE_BYTE_TO_MOUSE))
+            return 1;
+        // Write the cmd that will be sent to the mouse itself
+        if (kbc_send_cmd(MOUSE_IN_BUF, cmd))
+            return 1;
+
+        // Make sure the mouse responded with ACK
+        uint8_t byte_received;
+        if (kbc_read_outbf(MOUSE_OUT_BUF, &byte_received))
+            return 1;
+        
+        // If ACK return 0, it's all fine
+        if (byte_received == MOUSE_CTRL_ACK)
+            return 0;
+        
+        // If NACK, retry again
+        if (byte_received == MOUSE_CTRL_NACK) {
+            --attempts;
+            continue;
+        }
+        
+        // If it was anything else... Something is REALLY wrong, return 1
         return 1;
-    
-    return 0;
+    }
+
+    return 1;
 }
 
+// Enables mouse data reporting
 uint8_t mouse_data_reporting_enable() {
-    if (mouse_send_cmd(MOUSE_CMD_ENABLE_DATA_REPORT))
-        return 1;
-    return 0;
+    return mouse_send_cmd(MOUSE_CMD_ENABLE_DATA_REPORT);
 }
 
+// Disables mosue data reporting
 uint8_t mouse_data_reporting_disable() {
-    if (mouse_send_cmd(MOUSE_CMD_DISABLE_DATA_REPORT))
-        return 1;
-    return 0;
+    return mouse_send_cmd(MOUSE_CMD_DISABLE_DATA_REPORT);
 }
 
+// For now unused, but just garantees that data reporting was
+// disabled before sending the command, reenabling it afterwards
 uint8_t mouse_send_cmd_stream_mode(uint8_t cmd) {
     if (mouse_data_reporting_disable())
         return 1;
@@ -65,10 +89,19 @@ uint8_t mouse_send_cmd_stream_mode(uint8_t cmd) {
     return 0;
 }
 
+// Sets mouse to stream mode
 uint8_t mouse_set_stream_mode() {
-    if (mouse_send_cmd(MOUSE_CMD_STREAM_MODE))
-        return 1;
-    return 0;
+    return mouse_send_cmd(MOUSE_CMD_STREAM_MODE);
+}
+
+// Sets mouse to remote mode
+uint8_t mouse_set_remote_mode() {
+    return mouse_send_cmd(MOUSE_CMD_REMOTE_MODE);
+}
+
+// Sends cmd to mouse signaling we want to read a packet
+uint8_t mouse_read_data() {
+    return mouse_send_cmd(MOUSE_CMD_READ_DATA);
 }
 
 void (mouse_ih)() {
@@ -96,6 +129,7 @@ void (mouse_ih)() {
 
 uint8_t (parse_packet)() {
 
+    // Copy raw bytes to the struct
     mouse_parsed_packet.bytes[0] = packet_bytes[0];
     mouse_parsed_packet.bytes[1] = packet_bytes[1];
     mouse_parsed_packet.bytes[2] = packet_bytes[2]; 
@@ -143,6 +177,7 @@ uint8_t mouse_data_handler() {
         return 0;
     }
     else { // We need to find out the first byte
+        // Assume that if that bit is 1, it's the first byte/ We need to find out the first byte
         // Assume that if that bit is 1, it's the first byte
         is_mouse_packet_complete = false;
         if (packet_bytes[counter] & MOUSE_PARSE_FIRST_PACKET_IDENTIFIER) {
