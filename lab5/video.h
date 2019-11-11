7ccd314444addd28d00a85fe72c757177c1581c9
up201806433 <up201806433@1742a6f7-5c60-4130-9989-5d8d78b829ae>
2019-11-11 19:23:28 +0000

    video_test_init() done NOTE: Passes tests in all modes

---
+++ lab5/video.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <lcom/lcf.h>
+#include "video_macros.h"
+
+void privctl();
+
+int video_init(uint16_t mode);
---
+++ lab5/video_macros.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <lcom/lcf.h>
+
+#define MEM_MAX_SIZE 8000000;
