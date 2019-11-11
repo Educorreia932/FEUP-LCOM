4583eeb2245da707bf6c18780067d8f19b05255e
Tiago Duarte da Silva <48838198+tiagodusilva@users.noreply.github.com>
2019-11-11 17:45:31 +0000

    Added the remaining files

---
+++ lab5/i8042.h
@@ -0,0 +1,111 @@
+#ifndef _LCOM_I8042_H_
+#define _LCOM_I8042_H_
+
+#include <lcom/lcf.h>
+
+/** @defgroup i8042 i8042
+ * @{
+ *
+ * Constants for programming the i8042 Keyboard/Mouse.
+ */
+
+
+/* ---------------- Keyboard Macros ---------------- */
+
+#define KBD_IRQ 1 /**< @brief Keyboard IRQ line */
+
+/* Time outs */
+#define TIMEOUT_ATTEMPTS 5 // At a rate of 10ms per attempt, it will timeout in 50ms
+#define KBC_WAIT 10000 // Will wait for 10ms
+
+/* I/O Port addresses */
+
+#define OUT_BUF 0x60
+#define IN_BUF_CMD 0x64
+#define IN_BUF_ARGS 0x60
+#define STAT_REG 0x64
+
+/* KBD Commands */
+
+#define READ_CMD_BYTE 0x20
+#define WRITE_CMD_BYTE 0x60
+#define SELF_TEST 0xAA
+#define CHECK_KBD_INTERFACE 0xAB
+#define ENABLE_KBD_INTERFACE 0xAD
+#define DISABLE_KBD_INTERFACE 0xAE
+
+/* KBD Command Byte */
+
+#define CMD_BYTE_DISABLE_MOUSE BIT(5)
+#define CMD_BYTE_DISABLE_KBD_INTERFACE BIT(4)
+#define CMD_BYTE_ENABLE_INT_MOUSE BIT(1)
+#define CMD_BYTE_ENABLE_INT_KBD BIT(0)
+
+/* Status Related Macros */
+/* Used to "filter" the relevant bits from the status byte */
+
+/* Input buffer full */
+
+#define ST_OUT_BUF BIT(0)
+#define ST_IN_BUF BIT(1)
+
+/* Is the output buffer filled with mouse data */
+
+#define ST_MOUSE_DATA BIT(5)
+
+/* Error selection: bits 7 and 6 */
+
+#define ST_PAR_ERR BIT(7) 
+#define ST_TO_ERR BIT(6)
+
+/* Scancode Related Macros */
+#define ESC_BREAKCODE 0x81
+#define MAKE_TO_BREAK 0x80 /**< @brief Difference between the breakcode and the makecode */
+
+#define SCANCODE_TWO_BYTES 0xE0
+
+/* ---------------- Mouse Macros ---------------- */
+
+#define MOUSE_IRQ 12 /**< @brief Mouse IRQ line */
+
+/* PS/2 Mouse Ports */
+
+#define MOUSE_STAT_REG 0x64
+#define MOUSE_CTRL_REG 0x64
+#define MOUSE_IN_BUF 0x60
+#define MOUSE_OUT_BUF 0x60
+
+/* Mouse Commands */
+
+#define KBC_WRITE_BYTE_TO_MOUSE 0xD4
+#define MOUSE_CMD_RESET 0xFF
+#define MOUSE_CMD_DISABLE_DATA_REPORT 0XF5
+#define MOUSE_CMD_ENABLE_DATA_REPORT 0xF4
+#define MOUSE_CMD_REMOTE_MODE 0xF0
+#define MOUSE_CMD_READ_DATA 0xEB
+#define MOUSE_CMD_STREAM_MODE 0xEA
+#define MOUSE_CMD_SCALING_ACCELERATION 0xE7
+#define MOUSE_CMD_SCALING_LINEAR 0xE6
+#define MOUSE_CMD_STATUS_REQUEST 0xE9
+
+/* Mouse Controller Acknowledgements */
+
+#define MOUSE_CTRL_ACK 0xFA
+#define MOUSE_CTRL_NACK 0xFE
+#define MOUSE_CTRL_ERROR 0xFC
+
+/* Mouse Packet Parsing */
+
+#define MOUSE_PARSE_LB BIT(0)
+#define MOUSE_PARSE_RB BIT(1)
+#define MOUSE_PARSE_MB BIT(2)
+#define MOUSE_PARSE_FIRST_PACKET_IDENTIFIER BIT(3)
+#define MOUSE_PARSE_X_DELTA_MSB BIT(4)
+#define MOUSE_PARSE_Y_DELTA_MSB BIT(5)
+#define MOUSE_PARSE_X_OVERFLOW BIT(6)
+#define MOUSE_PARSE_Y_OVERFLOW BIT(7)
+#define EXTEND_SIGN 0xFF00
+
+/**@}*/
+
+#endif /* _LCOM_I8042_H */
---
+++ lab5/timer.c
@@ -0,0 +1,209 @@
+#include <lcom/lcf.h>
+#include <lcom/timer.h>
+
+#include <stdint.h>
+
+#include "timer.h"
+
+static int timer0_hook_id;
+unsigned long int global_timer0_counter = 0;
+
+// Sets the corresponding timer bits on the control word
+int timer_set_control_word_timer(uint8_t timer, uint8_t *cmd)
+{
+  if (!cmd) // Check if pointer is NULL
+    return 1;
+
+  switch (timer)
+  {
+  case 0:
+    *cmd |= TIMER_SEL0;
+    return 0;
+  case 1:
+    *cmd |= TIMER_SEL1;
+    return 0;
+  case 2:
+    *cmd |= TIMER_SEL2;
+    return 0;
+  default:
+    return 1;
+  }
+}
+
+// Sets timer_port to the correct port of the timer
+// Returns 0 if timer is between 0 and 2, returns 1 otherwise
+int timer_get_port(uint8_t timer, uint8_t *timer_port)
+{
+  if (!timer_port) // Check if pointer is NULL
+    return 1;
+
+  switch (timer)
+  {
+    case 0:
+      *timer_port = TIMER_0;
+      return 0;
+    case 1:
+      *timer_port = TIMER_1;
+      return 0;
+    case 2:
+      *timer_port = TIMER_2;
+      return 0;
+    default:
+      return 1;
+  }
+}
+
+int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
+    // Freq lower than 19 would get a div larger than 16 bit -> Error
+    // Freq higher than TIMER_FREQ (the clock) it is impossible -> Error
+    if (freq < 19 || freq > TIMER_FREQ)
+      return 1;
+
+  // "Thus, in mode 3, the timer generates a square wave with a frequency given by the expression clock/div, where clock is the frequency of the Clock input and div is the value loaded initially in the timer."
+  // TIMER_FREQ is clock on that equation
+  // div is Time-Base
+  uint16_t div = TIMER_FREQ / freq;
+
+  uint8_t cmd; // Will initially have Status
+  timer_get_conf(timer, &cmd); // Loads status to cmd
+
+  // Saves the 4 least significant bits to preserve mode (3) and counting (BCD/Binary)
+  cmd &= 0x0F;
+
+  // Sets the correct timer on the control word
+  if (timer_set_control_word_timer(timer, &cmd))
+    return 1;
+  
+  // Sets LSB followed by MSB on the control word
+  cmd |= TIMER_LSB_MSB;
+
+  // Gets the timer port
+  uint8_t timer_port;
+  if (timer_get_port(timer, &timer_port))
+    return 1;
+
+  // Gets the LSB and MSB of the div
+  uint8_t lsb, msb;
+  if (util_get_LSB(div, &lsb))
+    return 1;
+  if (util_get_MSB(div, &msb))
+    return 1;
+
+  // Sends control word
+  if (sys_outb(TIMER_CTRL, cmd))
+    return 1;
+
+  //Send the div to get the frequency right
+  if (sys_outb(timer_port, lsb))
+    return 1;
+  if (sys_outb(timer_port, msb))
+    return 1;
+
+  return 0;
+}
+
+// Sends the bit number for the interrupt through bit_no
+// and saves the hook id on timer0_hook_id to be used
+// later for unsubscribing and other actions
+int (timer_subscribe_int)(uint8_t *bit_no) {
+
+  if (!bit_no) // Check if pointer is NULL
+    return 1;
+
+  *bit_no = TIMER0_IRQ;
+  timer0_hook_id = TIMER0_IRQ;
+  global_timer0_counter = 0; // Reset time counter
+
+  if (sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &timer0_hook_id) != OK)
+    return 1;
+
+  // Is not necessary but gives a fun message if uncommented :D
+  // if (sys_irqenable(&timer0_hook_id) != OK)
+  //   return 1;
+
+  return 0;
+}
+
+int (timer_unsubscribe_int)() {
+  if (sys_irqrmpolicy(&timer0_hook_id) != OK)
+    return 1;
+  return 0;
+}
+
+void (timer_int_handler)() {
+  ++global_timer0_counter;
+}
+
+
+int (timer_get_conf)(uint8_t timer, uint8_t *st) {
+
+  if (!st) // Checks if pointer is NULL
+    return 1;
+
+  uint8_t timer_port;
+  // Need to set the RB_COUNT to 1, otherwise the status will not be correct
+  uint8_t cmd = (uint8_t) (TIMER_RB_SEL(timer) |
+    TIMER_RB_CMD | TIMER_RB_COUNT_);
+  
+  if (timer_get_port(timer, &timer_port))
+    return 1;
+  
+  if (sys_outb(TIMER_CTRL, cmd))
+    return 1;
+  
+  return(util_sys_inb(timer_port, st));
+}
+
+
+
+int (timer_display_conf)(uint8_t timer, uint8_t st,
+                        enum timer_status_field field) {
+  
+  // Check if timer is within range
+  if (timer > 2)
+    return 1;
+
+  union timer_status_field_val field_val;
+  if (field == 0) // aka "all"
+  {
+    field_val.byte = st;
+
+  } else if (field == 1) // aka counting "mode" (0 - 5)
+  {
+    //  Selects only the counting mode bits and shifts them to get simpler numbers
+    st = (st & TIMER_STATUS_TYPE_OF_ACCESS) >> 4;
+    switch (st)
+    {
+      case INVAL_val: // 000
+      case LSB_only: // 001
+      case MSB_only: // 010
+      case MSB_after_LSB: // 011
+        field_val.in_mode = st; break;
+      default: // If not a valid value, returns 1
+        return 1;
+    }
+
+  } else if (field == 2) // aka "init"ialization mode (LSB and MSB related)
+  {
+    // >> 1 Because the programmed mode is in binary representraion,
+    // but on the bits 1, 2 and 3 (if the TIMER_STATUS_PROGRAMMED_MODE is unchanged)
+    field_val.count_mode = (st & TIMER_STATUS_PROGRAMMED_MODE) >> 1;
+    // If mode = {2, 3} it would read {6, 7}, so this fixes that
+    if (field_val.count_mode & BIT(1))
+    {
+      field_val.count_mode &= ~BIT(2); // Sets BIT 2 to 0
+    }
+
+  } else if (field == 3) // aka counting "base"
+  {
+    // VGets the counting base and sends the result to the field_val bool
+    if (st & TIMER_STATUS_BCD)
+      field_val.bcd = true;
+    else
+      field_val.bcd = false;
+
+  } else
+      return 1;
+
+  return timer_print_config(timer, field, field_val);
+}
