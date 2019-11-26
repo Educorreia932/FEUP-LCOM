048eeeea1842c63e8508db57c44f47ec894247ff
up201806516 <up201806516@1742a6f7-5c60-4130-9989-5d8d78b829ae>
2019-11-26 17:31:23 +0000

    Added cursor, but the sprite is quite odd

--- proj/game_manager.c
+++ proj/game_manager.c
@@ -1,84 +1,99 @@
 #include "game_manager.h"
 #include "hw_manager.h"
 #include "level.h"
-#include "video.h"
+#include "mouse_cursor.h"
 
 struct GameManager {
-    Level_t *level;
-    MouseInputEvents_t *mouse_input_events;
-    KbdInputEvents_t *kbd_input_events;
+	MouseInputEvents_t *mouse_ev;
+	KbdInputEvents_t *kbd_ev;
+	Level_t *level;
+	MouseCursor_t *cursor;
 };
 
 GameManager_t * new_game_manager(const char* background_file_name) {
-  GameManager_t *gm = (GameManager_t*) malloc(sizeof(GameManager_t));
-  if (gm == NULL) {
-    printf("new_game_manager: Failed to allocate memory for GameManager object\n");
-    return NULL;
-  }
+	GameManager_t *gm = (GameManager_t*) malloc(sizeof(GameManager_t));
+	if (gm == NULL) {
+		printf("new_game_manager: Failed to allocate memory for GameManager object\n");
+		return NULL;
+	}
 
-  gm->level = new_level(background_file_name);
-  if (gm->level == NULL) {
-    printf("new_level_manager: Failed to allocate memory for the Level object\n");
-    return NULL;
-  }
+	gm->kbd_ev = new_kbd_input_events();
+	if (gm->kbd_ev == NULL) {
+		printf("new_level_manager: Failed to create the Keyboard Input Events object\n");
+		return NULL;
+	}
 
-  gm->kbd_input_events = new_kbd_input_events();
-  if (gm->kbd_input_events == NULL) {
-    printf("new_level_manager: Failed to allocate memory for the Keyboard Input Events object\n");
-    return NULL;
-  }
+	gm->mouse_ev = new_mouse_input_events();
+	if (gm->mouse_ev == NULL) {
+		printf("new_level_manager: Failed to create the Mouse Input Events object\n");
+		return NULL;
+	}
 
-  gm->mouse_input_events = new_mouse_input_events();
-  if (gm->mouse_input_events == NULL) {
-    printf("new_level_manager: Failed to allocate memory for the Mouse Input Events object\n");
-    return NULL;
-  }
+	gm->level = new_level(background_file_name);
+	if (gm->level == NULL) {
+		printf("new_level_manager: Failed to create the Level object\n");
+		return NULL;
+	}
 
-  return gm;
+	gm->cursor = new_cursor(gm->mouse_ev, "/home/lcom/labs/proj/assets/cursor.bmp");
+	if (gm->cursor == NULL) {
+		printf("new_level_manager: Failed to create cursor object\n");
+		return NULL;
+	}
+
+	return gm;
 }
 
 GameManager_t * new_testing_game_manager() {
-  GameManager_t *gm = (GameManager_t*) malloc(sizeof(GameManager_t));
-  if (gm == NULL) {
-    printf("new_testing_game_manager: Failed to allocate memory for GameManager object\n");
-    return NULL;
-  }
+	GameManager_t *gm = (GameManager_t*) malloc(sizeof(GameManager_t));
+	if (gm == NULL) {
+		printf("new_testing_game_manager: Failed to allocate memory for GameManager object\n");
+		return NULL;
+	}
 
-  gm->level = new_testing_level();
-  if (gm->level == NULL) {
-    printf("new_testing_game_manager: Failed to allocate memory for the Level object\n");
-    return NULL;
-  }
+	gm->kbd_ev = new_kbd_input_events();
+	if (gm->kbd_ev == NULL) {
+		printf("new_testing_game_manager: Failed to create the Keyboard Input Events object\n");
+		return NULL;
+	}
 
-  gm->kbd_input_events = new_kbd_input_events();
-  if (gm->kbd_input_events == NULL) {
-    printf("new_testing_game_manager: Failed to allocate memory for the Keyboard Input Events object\n");
-    return NULL;
-  }
+	gm->mouse_ev = new_mouse_input_events();
+	if (gm->mouse_ev == NULL) {
+		printf("new_testing_game_manager: Failed to create the Mouse Input Events object\n");
+		return NULL;
+	}
 
-  gm->mouse_input_events = new_mouse_input_events();
-  if (gm->mouse_input_events == NULL) {
-    printf("new_testing_game_manager: Failed to allocate memory for the Mouse Input Events object\n");
-    return NULL;
-  }
+	gm->level = new_testing_level();
+	if (gm->level == NULL) {
+		printf("new_testing_game_manager: Failed to create the Level object\n");
+		return NULL;
+	}
 
-  return gm;
+	gm->cursor = new_cursor(gm->mouse_ev, "/home/lcom/labs/proj/assets/cursor.bmp");
+	if (gm->cursor == NULL) {
+		printf("new_testing_game_manager: Failed to create the Cursor object\n");
+		return NULL;
+	}
+
+	return gm;
 }
 
 void free_game_manager(GameManager_t *gm) {
-  free_level(gm->level);
-  free_kbd_input_events(gm->kbd_input_events);
-  free_mouse_input_events(gm->mouse_input_events);
-  free(gm);
+	free_level(gm->level);
+	free_kbd_input_events(gm->kbd_ev);
+	free_mouse_input_events(gm->mouse_ev);
+	free(gm);
 }
 
 // Update is executed once every frame
 void update(GameManager_t* gm) {
-  	update_level(gm->level, gm->kbd_input_events, gm->mouse_input_events);
+  	update_level(gm->level, gm->kbd_ev, gm->mouse_ev);
+	update_cursor(gm->cursor);
 }
 
 void render(GameManager_t *gm) {
   	render_level(gm->level);
+	render_cursor(gm->cursor);
 }
 
 uint8_t start_game() {  
@@ -98,7 +113,7 @@ uint8_t start_game() {
 	uint32_t mouse_bit_mask;
 
   	if (hw_manager_subscribe_int(&timer0_bit_mask, &kbd_bit_mask, &mouse_bit_mask))
-    	printf("start_game: Failed to enable interrupts\n");
+		printf("start_game: Failed to enable interrupts\n");
   
   	printf("start_game: Subscribed to all interrupts\n");
 
@@ -110,7 +125,7 @@ uint8_t start_game() {
 	bool is_frame = false;
 	printf("start_game: Entering game loop\n");
 
-	while (!gm->kbd_input_events->key_esc_down) {
+	while (!gm->kbd_ev->key_esc_down) {
 		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
 			printf("start_game: driver_receive failed with: %d", r);
 			continue;
@@ -122,14 +137,14 @@ uint8_t start_game() {
 					if (msg.m_notify.interrupts & mouse_bit_mask) {
 						hw_manager_mouse_ih();
 
-			hw_manager_mouse(gm->mouse_input_events);
+			hw_manager_mouse(gm->mouse_ev);
 			
 		}
 					
 		if (msg.m_notify.interrupts & kbd_bit_mask) {
 			hw_manager_kbd_ih();
 
-			hw_manager_kbd(gm->kbd_input_events);
+			hw_manager_kbd(gm->kbd_ev);
 		}
 		
 		if (msg.m_notify.interrupts & timer0_bit_mask) {
@@ -147,8 +162,8 @@ uint8_t start_game() {
 		if (is_frame) {
 			update(gm);
 			render(gm);
-			reset_inputs(gm->kbd_input_events, gm->mouse_input_events);
-			switch_double_buffer();
+			reset_inputs(gm->kbd_ev, gm->mouse_ev);
+			hw_manager_switch_double_buffer();
 
 			is_frame = false;
 		}
---
+++ proj/mouse_cursor.c
@@ -0,0 +1,60 @@
+#include "mouse_cursor.h"
+#include "math.h"
+#include "sprite.h"
+#include "input_events.h"
+
+struct MouseCursor {
+    float x, y;
+    Sprite_t* sprite;
+    MouseInputEvents_t* mouse_ev;
+};
+
+/* CONSTRUCTORS */
+MouseCursor_t* new_cursor(MouseInputEvents_t *mouse_ev, const char* mouse_bmp_file_name) {
+    MouseCursor_t* cursor = (MouseCursor_t*)  malloc(sizeof(MouseCursor_t));
+    if (cursor == NULL) {
+        printf("new_cursor: Failed to allocate memory for the new Cursor\n");
+        return NULL;
+    }
+
+    if (mouse_ev == NULL) {
+        printf("new_cursor: MouseInputEvents_t must be valid (it was a NULL pointer)\n");
+        return NULL;
+    }
+    cursor->mouse_ev = mouse_ev;
+
+    // TODO: Use screen size
+    cursor->x = 100;
+    cursor->y = 100;
+    
+    cursor->sprite = new_sprite(0.0f, 0.0f, 1, mouse_bmp_file_name);
+    if (cursor->sprite == NULL) {
+        printf("new_testing_cursor: Failed to create the Cursor's Sprite\n");
+        return NULL;
+    }
+
+    return cursor;
+}
+
+void free_cursor(MouseCursor_t* cursor) {
+    free_sprite(cursor->sprite);
+    free(cursor);
+}
+
+/* CURSOR METHODS */
+
+void update_cursor(MouseCursor_t* cursor) {
+    cursor->x = fclamp(
+        cursor->x + cursor->mouse_ev->x_delta,
+        0,
+        1024
+    );
+    cursor->y = fclamp(
+        cursor->y - cursor->mouse_ev->y_delta,
+        0,
+        768
+    );
+}
+void render_cursor(MouseCursor_t* cursor) {
+    draw_sprite_floats(cursor->sprite, cursor->x, cursor->y, COLOR_NO_MULTIPLY);
+}
