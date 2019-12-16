198df15cc4b2526af30eac43202952c290475d12
up201806433 <up201806433@1742a6f7-5c60-4130-9989-5d8d78b829ae>
2019-12-16 18:09:08 +0000

    Added UART files

---
+++ proj/uart.c
@@ -0,0 +1,190 @@
+#include "uart.h"
+
+static int ser_hook_id;
+
+uint8_t (ser_subscribe_int)(uint8_t *bit_no) {
+    if (!bit_no) // Check if pointer is NULL
+        return 1;
+
+    ser_hook_id = SER_1_IRQ;
+    *bit_no = SER_1_IRQ;	
+
+    return sys_irqsetpolicy(SER_1_IRQ, IRQ_EXCLUSIVE | IRQ_REENABLE, &ser_hook_id);
+}
+
+uint8_t (ser_unsubscribe_int)() {
+    return sys_irqrmpolicy(&ser_hook_id);
+}
+
+int ser_test_conf(uint16_t base_addr) {
+    uint8_t lcr, ier;
+
+    if (util_sys_inb(base_addr + LCR, &lcr))
+        return 1;
+    
+    if (util_sys_inb(base_addr + IER, &ier))
+        return 1;
+
+    printf("LCR: %x\n", lcr);
+
+    uint8_t no_bits_per_char = lcr & NO_BITS_PER_CHAR;
+
+    printf("Number of bits per char:\n");
+
+    switch (no_bits_per_char) {
+        case 0: // 0b00
+            printf("\t5 bits per char\n");
+            break;
+        case 1: // 0b01
+            printf("\t6 bits per char\n");
+        case 2: // 0b10
+            printf("\t7 bits per char\n");
+        case 3: // 0b11
+            printf("\t8 bits per char\n");
+    }
+
+    uint8_t stop_bit = (lcr & STOP_BIT) >> 2;
+
+    printf("Number of stop bits:\n");
+
+    switch (stop_bit) {
+        case 0:
+            printf("\t1 stop bit\n");
+            break;
+        case 1:
+            printf("\t2 stop bits\n");
+            break;
+    }
+
+    uint8_t parity = (lcr & PARITY_CONTROL) >> 3;
+
+    printf("Parity control:\n");
+
+    switch (parity) {
+        case 1: // 0b001
+            printf("\tOdd parity\n");
+            break;
+        case 3: // 0b011
+            printf("\tEven parity\n");
+            break;
+        case 5: // 0b101
+            printf("\tParity bit is 1\n");
+            break;
+        case 7: // 0b111
+            printf("\tParity bit is 0\n");
+            break;
+        default: // 0bxx0
+            printf("\tNo parity\n");
+            break;
+    }
+
+    uint8_t dlab = (lcr & DLAB) >> 7;
+
+    printf("Divisor Latch Access\n");
+
+    switch (dlab) {
+        case 0:
+            printf("\tRBR (read)\n");
+            break;
+        case 1:
+            printf("\tTHR (write)\n");
+            break;
+    }
+
+    // TODO: IER
+
+    return 0;
+}
+
+int ser_test_set(uint16_t base_addr, uint8_t bits, uint8_t stop, int8_t parity, uint8_t rate) {
+    uint8_t lcr;
+
+    if (util_sys_inb(base_addr + LCR, &lcr))
+            return 1;
+
+    lcr &= 0xC0; // We don't to change bits 6 and 7
+
+    // Parity
+
+    switch (parity) {
+        case -1: // None
+            parity = 0;
+            break;
+        case 0: // Odd parity
+            parity = 1;
+            break;
+        case 1: // Even parity
+            parity = 3;
+            break;
+    }
+
+    // TODO: Rate
+
+    rate = 115200 / rate;
+
+    lcr |= bits | stop << 2 | parity << 3;
+
+    if (sys_outb(base_addr + LCR, lcr))
+        return 1;
+
+    return 0;
+}
+
+int ser_test_poll(uint16_t base_addr, uint8_t tx, uint8_t bits, uint8_t stop, int8_t parity, uint8_t rate, int stringc, char* strings[]) {
+    uint8_t delay = 100000 / bits;
+
+    if (!tx) {
+        if (receive_data(base_addr, delay))
+            return 1;
+    }
+        
+    else
+        if (send_data(base_addr, stringc, strings, delay))
+            return 1;
+
+    return 0;
+}
+
+int receive_data(uint16_t base_addr, uint8_t delay) {
+    uint8_t lsr;
+    uint8_t character; 
+
+    while (true) {
+        if (util_sys_inb(base_addr + LSR, &lsr))
+            return 1;
+
+        while(!(lsr & THR_EMPTY)) {
+            tickdelay(micros_to_ticks(delay));
+            
+            if (util_sys_inb(base_addr + LSR, &lsr))
+                return 1;
+        }
+
+        if (util_sys_inb(base_addr + RBR, &character))
+            return 1;
+
+        if (character == 1)
+            return 1;
+
+        if (character == '.')
+            return 0;
+    }
+}
+
+int send_data(uint8_t base_addr, int stringc, char *strings[], uint8_t delay) {
+    uint8_t lsr;
+    
+    for (int i = 0; i < stringc; i++) {
+        if (util_sys_inb(base_addr + LSR, &lsr) != OK)
+		    return 1;
+
+        if (lsr & OVERRUN_ERROR || lsr & PARITY_ERROR || lsr & FRAMING_ERROR)
+            return 1;
+
+        // Send character
+        if (sys_outb(base_addr + THR, strings[i]))
+            return 1;
+    }
+    
+    return 0;
+}
---
+++ proj/uart.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <lcom/lcf.h>
+
+#include "utils.h"
+
+#define SER_1_IRQ 3
+#define SER_2_IRQ 4
+
+#define COM1 0x3F8 
+#define COM2 0x2F8
+
+#define DLL 0 /**< @brief Divisor Latch LSB */
+#define DLM 1 /**< @brief Divisor Latch MSB */
+
+/** @name UART Accessible (8-bit) Registers */
+///@{
+#define RBR 0 /**< @brief Receiver Buffer Register */
+#define THR 0 /**< @brief Transmitter Holding Register */
+#define IER 1 /**< @brief Interrupt Enable Register */
+#define IIR 2 /**< @brief Interrupt Identification Register */
+#define FCR 2 /**< @brief FIFO Control Register */
+#define LCR 3 /**< @brief Line Control Register */
+#define MCR 4 /**< @brief Modem Control Register */
+#define LSR 5 /**< @brief Line Status Register */
+#define MSR 6 /**< @brief Modem Status Register */
+#define SR 7 /**< @brief Scratchpad Register */
+///@}
+
+/** @name Line Control Register (LCR) */
+///@{
+#define NO_BITS_PER_CHAR BIT(1) | BIT(0)
+#define STOP_BIT BIT(2)
+#define PARITY_CONTROL BIT(5) | BIT(4) | BIT(3)
+#define BREAK_CONTROL BIT(6)
+#define DLAB BIT(7)
+///@}
+
+/** @name Line Status Register (LSR) (Read only) */
+///@{
+#define RECEIVER_DATA BIT(0) /**< @brief Set to 1 when there is data for receiving */
+#define OVERRUN_ERROR BIT(1) /**< @brief  Set to 1 when a characters received is overwritten by another one */
+#define PARITY_ERROR BIT(2) /**< @brief Set to 1 when a character with a parity error is received */
+#define FRAMING_ERROR BIT(3) /**< @brief Set to 1 when a received character does not have a valid Stop bit */
+#define BREAK_INTERRUPT BIT(4) /**< @brief Set to 1 when the serial data input line is held in the low level for longer than a full “word” transmission */
+#define THR_EMPTY BIT(5) /**< @brief When set, means that the UART is ready to accept a new character for transmitting */
+#define THR_TSR_EMPTY BIT(6) /**< @brief When set, means that both the THR and the Transmitter Shift Register are both empty */
+#define FIFO_ERROR BIT(7) /**< @brief Set to 1 when there is at least one parity error or framing error or break indication in the FIFO Reset to 0 when the LSR is read, if there are no subsequent errors in the FIFO */
+///@}
+
+#define SER_OVERRUN_ERR BIT(1)
+#define SER_PARITY_ERR BIT(2)
+#define SER_FRAME_ERR BIT(3)
+
+#define SER_DATA 0
+#define SER_RX_RDY BIT(0)
+
+uint8_t (ser_subscribe_int)(uint8_t *bit_no);
+uint8_t (ser_unsubscribe_int)();
+
+/** 
+ * @brief Read the control/status registers of the UART.
+ * @details And displays it in a convenient format for humans to read.
+ */
+int ser_test_conf(uint16_t base_addr);
+
+/**
+ * @param bits Number of bits per character
+ * @param stop Number of stop bits
+ * @param parity -1, 0 or 1
+ * @param rate Bit-rate
+ */
+int ser_test_set(uint16_t base_addr, uint8_t bits, uint8_t stop, int8_t parity, uint8_t rate);
+
+/**
+ * @brief Use the UART in polled mode for simplex communication
+ * @param tx Role (transmitter or receiver)
+ * @param bits Number of bits per character
+ * @param stop Number of stop bits
+ * @param parity -1, 0 or 1
+ * @param rate Bit-rate
+ */
+int ser_test_poll(uint16_t base_addr, uint8_t tx, uint8_t bits, uint8_t stop, int8_t parity, uint8_t rate, int stringc, char *strings[]);
+
+int receive_data(uint16_t base_addr, uint8_t delay);
+
+int send_data(uint8_t base_addr, int stringc, char *strings[], uint8_t delay);
+
+int ser_test_int();
+
+int ser_test_fifo();
+
