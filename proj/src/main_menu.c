97de8f0d06abfab4ee1b9efe895794b1d8fc0a98
up201806516 <up201806516@1742a6f7-5c60-4130-9989-5d8d78b829ae>
2019-12-30 19:23:20 +0000

    Divided campaign coop into two parts (main menu). Added versus arcade button (does nothing so far though)

--- proj/src/main_menu.c
+++ proj/src/main_menu.c
@@ -5,7 +5,7 @@
 
 struct MainMenu {
     Sprite_t *background;
-    Button_t *camp_single, *camp_coop, *arcade_single, *arcade_versus;
+    Button_t *camp_single, *camp_coop_p2, *camp_coop_p1, *arcade_single, *arcade_versus;
     // Button_t *camp_single_h, *camp_coop_h, *arcade_single_h, *arcade_coop_h;
 };
 
@@ -15,10 +15,16 @@ static void menu_start_campaign_single() {
     gm_start_level();
 }
 
-static void menu_start_campaing_coop() {
+static void menu_start_campaign_coop_p1() {
     GameManager_t* gm = get_game_manager();
     gm->gamemode = GM_LEVEL_UART;
     gm_start_level();
+}
+
+static void menu_start_campaign_coop_p2() {
+    GameManager_t* gm = get_game_manager();
+    gm->gamemode = GM_SWITCHBOARD_UART;
+    gm_start_switchboard();
 }
 
 static void menu_start_arcade_single() {
@@ -27,11 +33,11 @@ static void menu_start_arcade_single() {
     gm_start_arcade();
 }
 
-// static void menu_start_arcade_versus() {
-//     GameManager_t* gm = get_game_manager();
-//     gm->gamemode = GM_ARCADE_UART;
-//     gm_start_arcade();
-// }
+static void menu_start_arcade_versus() {
+    // GameManager_t* gm = get_game_manager();
+    // gm->gamemode = GM_ARCADE_UART;
+    // gm_start_arcade();
+}
 
 MainMenu_t* new_main_menu() {
     MainMenu_t* menu = (MainMenu_t*) malloc(sizeof(MainMenu_t));
@@ -47,10 +53,11 @@ MainMenu_t* new_main_menu() {
         return NULL;
     }
 
-    menu->camp_single = new_button("main_menu/single_button.bmp", menu_start_campaign_single, rect(75, 490, 216, 75));
-    menu->camp_coop = new_button("main_menu/coop_button.bmp", menu_start_campaing_coop, rect(281, 490, 216, 75));
-    menu->arcade_single = new_button("main_menu/single_button.bmp", menu_start_arcade_single, rect(545, 490, 216, 75));
-    // menu->arcade_versus;
+    menu->camp_single = new_button("main_menu/single_button.bmp", menu_start_campaign_single, rect(75, 490, 207, 75));
+    menu->camp_coop_p1 = new_button("main_menu/coop_p1_button.bmp", menu_start_campaign_coop_p1, rect(281, 490, 216, 75));
+    menu->camp_coop_p2 = new_button("main_menu/coop_p2_button.bmp", menu_start_campaign_coop_p2, rect(279, 563, 218, 75));
+    menu->arcade_single = new_button("main_menu/single_button.bmp", menu_start_arcade_single, rect(545, 490, 207, 75));
+    menu->arcade_versus = new_button("main_menu/versus_button.bmp", menu_start_arcade_versus, rect(751, 490, 216, 75));
 
     return menu;
 
@@ -66,18 +73,20 @@ void free_main_menu(MainMenu_t *menu) {
     free_sprite(menu->background);
 
     free_button(menu->camp_single);
-    free_button(menu->camp_coop);
+    free_button(menu->camp_coop_p1);
+    free_button(menu->camp_coop_p2);
     free_button(menu->arcade_single);
-    // free_button(menu->arcade_versus);
+    free_button(menu->arcade_versus);
 
 }
 
 void update_main_menu(MainMenu_t *menu) {
 
     update_button(menu->camp_single);
-    update_button(menu->camp_coop);
+    update_button(menu->camp_coop_p1);
+    update_button(menu->camp_coop_p2);
     update_button(menu->arcade_single);
-    // update_button();
+    update_button(menu->arcade_versus);
 
 }
 
@@ -86,8 +95,9 @@ void render_main_menu(MainMenu_t *menu) {
     draw_sprite_floats(menu->background, 0, 0, COLOR_NO_MULTIPLY, SPRITE_NORMAL);
 
     render_button(menu->camp_single);
-    render_button(menu->camp_coop);
+    render_button(menu->camp_coop_p1);
+    render_button(menu->camp_coop_p2);
     render_button(menu->arcade_single);
-    // render_button(menu->arcade_versus);
+    render_button(menu->arcade_versus);
 
 }
