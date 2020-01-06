2aadf8954e9e53c06bb6b86709f5d7953dc14c6c
Tiago Duarte da Silva <48838198+tiagodusilva@users.noreply.github.com>
2020-01-06 17:38:32 +0000

    Added tutorials

--- proj/src/main_menu.h
+++ proj/src/main_menu.h
@@ -36,10 +36,11 @@ void free_main_menu(MainMenu_t* menu);
 /**
  * @brief Updates the needed elements of the main menu
  * @description Must be called every frame
- * 
- * @param menu Pointer to the Main Menu to be updated
+ * @note That pointer to pointer is here for a reason:\n
+ * As these buttons delete this Menu's instance, we need to know if it has been deleted, hence the pointer to pointer
+ * @param menu Pointer to pointer to the Main Menu to be updated
  */
-void update_main_menu(MainMenu_t *menu);
+void update_main_menu(MainMenu_t **menu);
 
 /**
  * @brief Renders the needed elements of the main menu
@@ -48,5 +49,21 @@ void update_main_menu(MainMenu_t *menu);
  * @param menu Pointer to the Main Menu to be rendered
  */
 void render_main_menu(MainMenu_t *menu);
+
+/**
+ * @brief Returns wether the menu is in a submenu (showing a tutorial)
+ * 
+ * @param menu Pointer to the menu to check
+ * @return True when the Menu is in a submenu (showing a tutorial)\n
+ * False otherwise
+ */
+bool main_menu_in_submenu(MainMenu_t *menu);
+
+/**
+ * @brief Exits a menu's submenu (the tutorials)
+ * 
+ * @param menu Pointer to the Menu
+ */
+void main_menu_exit_submenu(MainMenu_t *menu);
 
 /** @{ */
